e05d20ff2e2d04b6c9ba5641faaa8252786025e6
Evgeny Artemov <eartemov013@gmail.com>
2022-06-17 11:12:23 +0300

    add simple routing

--- App.mjs
+++ App.mjs
@@ -1,8 +1,19 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-  res.write('Welcome to our Home Page');
-  res.end();
+  if(req.url === '/') {
+    res.end('Welcome to our Home Page');
+    return;
+  }
+  if(req.url === '/about') {
+    res.end('Welcome to our About Page');
+    return;
+  }
+  res.end(`
+    <h1>404</h1>
+    <p>It seems that this page doesn't exist!</p>
+    <a href="/">Go back</a>
+  `)
 })
 
 server.listen(5002);
