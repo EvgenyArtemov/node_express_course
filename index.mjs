d4b2972cf2209cd82b91f69da8b1485a7f80fe37
Evgeny Artemov <eartemov013@gmail.com>
2022-06-19 14:27:35 +0300

    add modules

--- index.mjs
+++ index.mjs
@@ -1,10 +1,16 @@
 import { osModule } from './os-module.mjs';
 import { pathModule } from './path-module.mjs';
 import { fsModule } from './fs-module.mjs';
-import './async-fs-module.mjs'; // import default calls module internals 
+// import './async-fs-module.mjs'; // import default calls module internals 
 import './App.mjs';
+import './event-emitter-module.mjs';
+import { writeBigFile } from './content/big-fle.mjs'
+import './readStream-module.mjs';
 
-// osModule();
+osModule();
 // pathModule();
 // fsModule();
 // asyncFsModule();
+
+// generate really big file to read it later with read Stream B)
+writeBigFile();
