d9ed258da69b4768f6ead375a18a1babb09ab1b5
Evgeny Artemov <eartemov013@gmail.com>
2022-06-19 14:26:57 +0300

    generate the really big file

---
+++ content/big-fle.mjs
@@ -0,0 +1,7 @@
+import { writeFileSync } from 'fs';
+
+export const writeBigFile = () => {
+  for (let i = 0; i < 10000; i++) {
+    writeFileSync('./content/big.txt', `hello world ${i}\n`, { flag: 'a' })
+  }
+};
