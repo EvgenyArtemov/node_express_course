521db2d21fa4a8da3671ea6f0dfd3db8148125c5
Evgeny Artemov <eartemov013@gmail.com>
2022-08-02 13:16:59 +0300

    create simple server

--- App.mjs
+++ App.mjs
@@ -1,19 +1,28 @@
 import http from 'http';
+import { readFileSync } from 'fs';
+
+const homePage = readFileSync('./index.html');
 
 const server = http.createServer((req, res) => {
   if(req.url === '/') {
-    res.end('Welcome to our Home Page');
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write(homePage);
+    res.end();
     return;
   }
   if(req.url === '/about') {
-    res.end('Welcome to our About Page');
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('Welcome to our About Page');
+    res.end()
     return;
   }
-  res.end(`
+  res.writeHead(404, {'content-type': 'text-html'})
+  res.write(`
     <h1>404</h1>
     <p>It seems that this page doesn't exist!</p>
     <a href="/">Go back</a>
-  `)
+  `);
+  res.end();
 })
 
 server.listen(5002);
